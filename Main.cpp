034cf523a9c14f25ccc515cd81308148bbd15fa1
Ashwanth2310 <ashwanth@rajakumar.net>
2024-08-29 14:44:52 +0530

    New and delete

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include "Product.cpp"
-#include "VendingMachine.cpp"
+#include "VendingMachine.cpp"  
 
 int main() {
-    Product products[3] = {
-        Product("Chips", 1.50, 345),
-        Product("Coke", 2.00, 15),
-        Product("Feastables", 1.00, 27)
-    };
-    VendingMachine vm;
+
+    VendingMachine* vm = new VendingMachine();
+
+    Product* products[3];
+    products[0] = new Product("Chips", 1.50, 10);
+    products[1] = new Product("Sprite", 2.00, 15);
+    products[2] = new Product("Feastables", 1.00, 20);
+
 
     for (int i = 0; i < 3; ++i) {
-        vm.addProduct(&products[i]);
+        vm->addProduct(products[i]);
     }
-    
-    vm.displayProducts();
+
+    vm->displayProducts();
+
+    delete vm; 
 
     return 0;
 }
