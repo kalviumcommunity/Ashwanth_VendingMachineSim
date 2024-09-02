b4f994729d204797597b3c9be4f13f4b2d035f34
Ashwanth2310 <ashwanth@rajakumar.net>
2024-09-02 15:02:03 +0530

    Static Variables

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-#include "Product.cpp"  
+#include "Product.cpp"
 
 class VendingMachine {
 private:
-    Product* products[10];  
+    Product* products[10];  // Array of pointers to Product objects
     int productCount;
+    static double totalSales;  // Static variable to keep track of total sales
 
 public:
     VendingMachine() : productCount(0) {
@@ -16,7 +17,7 @@ public:
 
     VendingMachine& addProduct(Product* p) {
         if (productCount < 10) {
-            this->products[productCount] = p;
+            products[productCount] = p;
             productCount++;
         } else {
             std::cout << "Vending machine is full!" << std::endl;
@@ -24,19 +25,38 @@ public:
         return *this;
     }
 
+    void sellProduct(int index) {
+        if (index >= 0 && index < productCount && products[index] != nullptr) {
+            totalSales += products[index]->getPrice();
+            delete products[index];
+            products[index] = nullptr;
+            std::cout << "Product sold!" << std::endl;
+        } else {
+            std::cout << "Invalid product index." << std::endl;
+        }
+    }
+
     void displayProducts() {
         if (productCount == 0) {
             std::cout << "No products available." << std::endl;
         } else {
             for (int i = 0; i < productCount; ++i) {
-                products[i]->displayInfo();
+                if (products[i] != nullptr) {
+                    products[i]->displayInfo();
+                }
             }
         }
     }
 
+    static double getTotalSales() {
+        return totalSales; 
+    }
+
     ~VendingMachine() {
         for (int i = 0; i < productCount; ++i) {
-            delete products[i]; 
+            delete products[i];
         }
     }
 };
+
+double VendingMachine::totalSales = 0.0;
