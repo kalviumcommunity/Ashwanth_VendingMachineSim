f3557a22527ad36f44be8fcf9a13dc3fccc6c66d
Ashwanth2310 <ashwanth@rajakumar.net>
2024-10-25 10:49:47 +0530

    Polymorphism

--- Product.cpp
+++ Product.cpp
@@ -8,8 +8,9 @@ public:
     double price;
     int quantity;
 
-
     Product(string n, double p, int q) : name(n), price(p), quantity(q) {}
+
+    Product(string n, double p) : name(n), price(p), quantity(0) {}
 
     virtual ~Product() {
         cout << "Product " << name << " is being deleted." << endl;
@@ -17,5 +18,9 @@ public:
 
     virtual void displayInfo() {
         cout << "Product: " << name << ", Price: " << price << ", Quantity: " << quantity << endl;
+    }
+
+    void displayInfo(string currency) {
+        cout << "Product: " << name << ", Price: " << price << " " << currency << ", Quantity: " << quantity << endl;
     }
 };
